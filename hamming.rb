b94150c695efe698319dbe3ebb4fb18dea1c2a2d
gkpatel461 <gkpatel461@gmail.com>
2017-01-16 11:53:15 +0530

    Update hamming.rb
    normal change

--- hamming.rb
+++ hamming.rb
@@ -1,16 +1,16 @@
 class Hamming
 
-    def self.compute(dna,rna)
-      if(dna.length!=rna.length)
+    def self.compute(dna1,dna2)
+      if(dna1.length!=dna2.length)
         raise ArgumentError, "The length not matched"
 
       else
         i=0
         total=0
-        dna=dna.split('')
-        rna=rna.split('')
-        until i>dna.length do
-          if dna[i]!=rna[i]
+        dna1=dna1.split('')
+        dna2=dna2.split('')
+        until i>dna1.length do
+          if dna1[i]!=dna2[i]
             total=total+1
 
           end
